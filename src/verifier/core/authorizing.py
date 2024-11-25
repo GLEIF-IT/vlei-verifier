93e62f99d882ddd123148e10ec10101366409b8a
Aidar Negimatzhanov <aydar.negimatzhanov@perfectart.com>
2024-11-25 14:34:50 +0100

    Add root of trust verification

--- src/verifier/core/authorizing.py
+++ src/verifier/core/authorizing.py
@@ -38,7 +38,7 @@ class Schema:
     OOR_SCHEMA = "EBNaNu-M9P5cgrnfl2Fvymy4E_jvxxyjb70PRtiANlJy"
     QVI_SCHEMA1 = "EFgnk_c08WmZGgv9_mpldibRuqFMTQN-rAgtD-TCOwbs"
     QVI_SCHEMA2 = "EBfdlu8R27Fbx-ehrqwImnK-8Cm79sqbAQ4MmvEAYqao"
-    
+
     schema_names = {}
     schema_names[ECR_AUTH_SCHEMA1] = "ECR_AUTH"
     schema_names[ECR_AUTH_SCHEMA2] = "ECR_AUTH"
@@ -128,19 +128,21 @@ class Authorizer:
             now = helping.nowUTC()
             age = now - datetime.datetime.fromisoformat(state.date)
             cred_state = None
-            if state.state == AUTH_EXPIRE and age > datetime.timedelta(seconds=self.TimeoutAuth*2):
+            if state.state == AUTH_EXPIRE and age > datetime.timedelta(seconds=self.TimeoutAuth * 2):
                 self.vdb.iss.rem(keys=(aid,))
             # We keep revoked credentials in the DB because their auth should never expire and the state
             # must always be AUTH_REVOKED to avoid logging in again with the older version of the credential
-            elif state.state != AUTH_REVOKED and state.state != AUTH_EXPIRE and age > datetime.timedelta(seconds=self.TimeoutAuth):
-                cred_state = CredProcessState(said=state.said, state=AUTH_EXPIRE, info=f"Cred state exceeded {self.TimeoutAuth}")
+            elif state.state != AUTH_REVOKED and state.state != AUTH_EXPIRE and age > datetime.timedelta(
+                    seconds=self.TimeoutAuth):
+                cred_state = CredProcessState(said=state.said, state=AUTH_EXPIRE,
+                                              info=f"Cred state exceeded {self.TimeoutAuth}")
                 self.vdb.iss.pin(keys=(aid,), val=cred_state)
                 print(
                     f"{state.said} for {aid}, has expired: {age} greater than {self.TimeoutAuth}"
                 )
             elif (
-                self.reger.saved.get(keys=(state.said,)) is not None
-                and state.state == CRED_CRYPT_VALID
+                    self.reger.saved.get(keys=(state.said,)) is not None
+                    and state.state == CRED_CRYPT_VALID
             ):
                 print(f"{state.said} for {aid}, {AUTH_PENDING}")
                 self.vdb.iss.rem(keys=(aid,))
@@ -148,7 +150,7 @@ class Authorizer:
                 creder = self.reger.creds.get(keys=(state.said,))
                 # are there multiple creds for the same said?
                 passed_cred_filters, info = self.cred_filters(creder)
-                if(passed_cred_filters):
+                if (passed_cred_filters):
                     cred_state = CredProcessState(said=state.said, state=AUTH_SUCCESS, info=info)
                     acct = Account(creder.attrib["i"], creder.said, creder.attrib["LEI"])
                     self.vdb.accts.pin(keys=(creder.attrib["i"],), val=acct)
@@ -159,7 +161,7 @@ class Authorizer:
                 # No need to process state.state == CRED_CRYPT_INVALID or state.state == AUTH_EXPIRE or state.state == AUTH_FAIL or state.state == AUTH_REVOKED or state.state == AUTH_SUCCESS:
                 continue
 
-    def cred_filters(self, creder) -> tuple[bool,str]:
+    def cred_filters(self, creder) -> tuple[bool, str]:
         """Process a fully verified engagement context role vLEI credential presentation
 
         1.  If the LEI filter is configured, ensure the LEI is in the list of acceptable LEIs
@@ -179,11 +181,11 @@ class Authorizer:
                 if Schema.schema_names.get(creder.schema):
                     res = False, f"Can't authorize cred with {Schema.schema_names[creder.schema]} schema"
                 else:
-                    res = False, f"Can't authorize cred with unknown schema {creder.schema}"                        
+                    res = False, f"Can't authorize cred with unknown schema {creder.schema}"
 
         if res[0]:
             if creder.issuer not in self.hby.kevers:
-                res = False, f"unknown issuer {creder.issuer}"        
+                res = False, f"unknown issuer {creder.issuer}"
             elif creder.attrib["i"] == None or creder.attrib["i"] not in self.hby.kevers:
                 print(f"unknown issuee {creder.attrib["i"]}")
             elif len(self.leis) > 0 and creder.attrib["LEI"] not in self.leis:
@@ -198,11 +200,11 @@ class Authorizer:
         print(f"Cred filter status {res[0]}, {res[1]}")
         return res
 
-    def chain_filters(self, creder) -> tuple[bool,str]:
+    def chain_filters(self, creder) -> tuple[bool, str]:
         cred_type = "NON_VLEI"
         chain_success = False
         chain_msg = f"Unknown credential schema type {creder.schema} not supported"
-        
+
         cred_type = Schema.schema_names.get(creder.schema)
         match creder.schema:
             case Schema.ECR_SCHEMA | Schema.ECR_SCHEMA_PROD:
@@ -212,7 +214,8 @@ class Authorizer:
                         Schema.ECR_AUTH_SCHEMA1: {"LEI": creder.attrib["LEI"]},
                         Schema.ECR_AUTH_SCHEMA2: {"LEI": creder.attrib["LEI"]}
                     }
-                    chain_success, chain_msg = self.edge_filters(cred_type, creder.edge["auth"], valid_edges=valid_edges)
+                    chain_success, chain_msg = self.edge_filters(cred_type, creder.edge["auth"],
+                                                                 valid_edges=valid_edges)
                 elif creder.edge.get("le"):
                     # The edge of the LE should come from the same LEI
                     valid_edges = {
@@ -221,7 +224,7 @@ class Authorizer:
                     }
                     chain_success, chain_msg = self.edge_filters(cred_type, creder.edge["le"], valid_edges=valid_edges)
                 else:
-                    chain_success, chain_msg = (False,f"Unexpected {cred_type} cred edge {creder.edge}")
+                    chain_success, chain_msg = (False, f"Unexpected {cred_type} cred edge {creder.edge}")
             case Schema.ECR_AUTH_SCHEMA1 | Schema.ECR_AUTH_SCHEMA2:
                 if creder.edge.get("le"):
                     # The edge of the LE should come from the same LEI
@@ -231,7 +234,7 @@ class Authorizer:
                     }
                     chain_success, chain_msg = self.edge_filters(cred_type, creder.edge["le"], valid_edges=valid_edges)
                 else:
-                    chain_success, chain_msg = (False,f"Unexpected {cred_type} cred edge {creder.edge}")
+                    chain_success, chain_msg = (False, f"Unexpected {cred_type} cred edge {creder.edge}")
             case Schema.LE_SCHEMA1 | Schema.LE_SCHEMA2:
                 valid_edges = {
                     Schema.QVI_SCHEMA1: None,
@@ -240,13 +243,17 @@ class Authorizer:
                 if creder.edge.get("qvi"):
                     chain_success, chain_msg = self.edge_filters(cred_type, creder.edge["qvi"], valid_edges)
                 else:
-                    chain_success, chain_msg = (False,f"Unexpected {cred_type} cred edge {creder.edge}")
+                    chain_success, chain_msg = (False, f"Unexpected {cred_type} cred edge {creder.edge}")
             case Schema.QVI_SCHEMA1 | Schema.QVI_SCHEMA2:
                 if creder.edge:
-                    chain_success, chain_msg = (False,f"Unexpected {cred_type} cred edge {creder.edge}")
+                    chain_success, chain_msg = (False, f"Unexpected {cred_type} cred edge {creder.edge}")
                 else:
-                    chain_success, chain_msg = (True, "QVI")
-            #TODO add logic related to GLEIF external and internal
+                    issuer_aid = creder.sad.get("i")
+                    if self.vdb.root.get(keys=(issuer_aid,)):
+                        chain_success, chain_msg = (True, "QVI")
+                    else:
+                        chain_success, chain_msg = (False, "The issuer of the QVI credential is not a valid Root Of Trust")
+            # TODO add logic related to GLEIF external and internal
             # case Schema.GLEIF_EXTERNAL_SCHEMA:
             #     cred_type = "GLEIF_EXTERNAL"
             #     chain_success, chain_msg = self.edge_filters(cred_type, creder.edge, [Schema.GLEIF_INTERNAL_SCHEMA])
@@ -254,9 +261,9 @@ class Authorizer:
             #     cred_type = "GLEIF_INTERNAL"
             case _:
                 print(f"{chain_msg}")
-            
+
         return chain_success, chain_msg
-    
+
     def edge_filters(self, cred_type: str, edge, valid_edges: dict):
         chain_msg = "Expected edge "
         chain_success = False
@@ -278,12 +285,12 @@ class Authorizer:
         else:
             chain_success = False
             chain_msg = f"{cred_type} should chain to schema {dict.keys()}, not {edge["s"]}"
-        
+
         if not chain_success:
             chain_msg = f"{cred_type} chain validation failed, " + chain_msg
-            
+
         return chain_success, chain_msg
-    
+
     def attr_filters(self, cred, valid_attrs: dict):
         for key in valid_attrs.keys():
             assert cred.attrib[key] == valid_attrs[key]
--- tests/integration/test_service.py
+++ tests/integration/test_service.py
@@ -113,79 +113,79 @@ def test_service_ecr(seeder):
         if exceptions:
             raise exceptions[0]
         
-# @pytest.mark.manual
-# def test_service_integration(seeder):
-#     with habbing.openHab(name="sid", temp=True, salt=b'0123456789abcdef') as (hby, hab):
-#
-#         seeder.seedSchema(db=hby.db)
-#         regery, registry, verifier, seqner = reg_and_verf(hby, hab, registryName="qvireg")
-#         qvicred = get_qvi_cred(issuer=hab.pre, recipient=hab.pre, schema=Schema.QVI_SCHEMA1, registry=registry, lei="875500ELOZEL05BVXV37")
-#         hab, qcrdntler, qsaid, qkmsgs, qtmsgs, qimsgs, qvimsgs = get_cred(hby, hab, regery, registry, verifier, Schema.QVI_SCHEMA1, qvicred, seqner)
-#
-#         qviedge = get_qvi_edge(qvicred.sad["d"], Schema.QVI_SCHEMA1)
-#
-#         leicred = get_lei_cred(issuer=hab.pre, recipient=hab.pre, schema=Schema.LE_SCHEMA1, registry=registry, sedge=qviedge, lei="875500ELOZEL05BVXV37")
-#         hab, lcrdntler, lsaid, lkmsgs, ltmsgs, limsgs, leimsgs = get_cred(hby, hab, regery, registry, verifier, Schema.LE_SCHEMA1, leicred, seqner)
-#
-#         #chained ecr auth cred
-#         eaedge = get_ecr_auth_edge(lsaid,Schema.LE_SCHEMA1)
-#
-#         eacred = get_ecr_auth_cred(aid=hab.pre, issuer=hab.pre, recipient=hab.pre, schema=Schema.ECR_AUTH_SCHEMA2, registry=registry, sedge=eaedge, lei="875500ELOZEL05BVXV37")
-#         hab, eacrdntler, easaid, eakmsgs, eatmsgs, eaimsgs, eamsgs = get_cred(hby, hab, regery, registry, verifier, Schema.ECR_AUTH_SCHEMA2, eacred, seqner)
-#
-#         #chained ecr auth cred
-#         ecredge = get_ecr_edge(easaid,Schema.ECR_AUTH_SCHEMA2)
-#
-#         ecr = get_ecr_cred(issuer=hab.pre, recipient=hab.pre, schema=Schema.ECR_SCHEMA, registry=registry, sedge=ecredge, lei="875500ELOZEL05BVXV37")
-#         hab, eccrdntler, ecsaid, eckmsgs, ectmsgs, ecimsgs, ecmsgs = get_cred(hby, hab, regery, registry, verifier, Schema.ECR_SCHEMA, ecr, seqner)
-#
-#         app = falcon.App(
-#             middleware=falcon.CORSMiddleware(
-#                 allow_origins='*',
-#                 allow_credentials='*',
-#                 expose_headers=['cesr-attachment', 'cesr-date', 'content-type']))
-#         vdb = basing.VerifierBaser(name=hby.name, temp=True)
-#         verifying.setup(app=app, hby=hby, vdb=vdb, reger=eccrdntler.rgy.reger)
-#         server = http.Server(port=port, app=app)
-#         httpServerDoer = http.ServerDoer(server=server)
-#         # class testCf:
-#         #     def get():
-#         #         return dict(LEIs=[f"{LEI1}",f"{LEI2}"])
-#         rootsCf = configing.Configer(name="verifier-config-public.json",
-#                             headDirPath="/home/aidar/Desktop/git/gleif/vlei-verifier/scripts",
-#                             base="",
-#                             temp=False, reopen=True, clear=False)
-#         authDoers = authorizing.setup(hby, vdb=vdb, reger=eccrdntler.rgy.reger, cf=rootsCf)
-#
-#         reportDoers = reporting.setup(app=app, hby=hby, vdb=vdb)
-#
-#         doers = authDoers + reportDoers + [httpServerDoer]
-#         limit = 0.25
-#         tock = 0.03125
-#         doist = doing.Doist(limit=limit, tock=tock)
-#         doist.doers = doers
-#         doist.enter()
-#         # assert len(doist.deeds) == 2
-#         # assert [val[1] for val in doist.deeds] == [0.0, 0.0]  #  retymes
-#         # for doer in doers:
-#         #     assert doer.baser.opened
-#         #     assert "_test/keri/db/test" in doer.baser.path
-#         try:
-#             doist.recur()
-#         except Exception as e:
-#             raise ValueError(f"Likely you have another service running on {port}")
-#
-#         issAndCred = bytearray()
-#         # issAndCred.extend(kmsgs)
-#         # issAndCred.extend(tmsgs)
-#         # issAndCred.extend(imsgs)
-#         issAndCred.extend(ecmsgs)
-#         acdc = issAndCred.decode("utf-8")
-#
-#         # use this for integration testing debugging sessions
-#         while True:
-#             time.sleep(1)
-#             doist.recur()
+@pytest.mark.manual
+def test_service_integration(seeder):
+    with habbing.openHab(name="sid", temp=True, salt=b'0123456789abcdef') as (hby, hab):
+
+        seeder.seedSchema(db=hby.db)
+        regery, registry, verifier, seqner = reg_and_verf(hby, hab, registryName="qvireg")
+        qvicred = get_qvi_cred(issuer=hab.pre, recipient=hab.pre, schema=Schema.QVI_SCHEMA1, registry=registry, lei="875500ELOZEL05BVXV37")
+        hab, qcrdntler, qsaid, qkmsgs, qtmsgs, qimsgs, qvimsgs = get_cred(hby, hab, regery, registry, verifier, Schema.QVI_SCHEMA1, qvicred, seqner)
+
+        qviedge = get_qvi_edge(qvicred.sad["d"], Schema.QVI_SCHEMA1)
+
+        leicred = get_lei_cred(issuer=hab.pre, recipient=hab.pre, schema=Schema.LE_SCHEMA1, registry=registry, sedge=qviedge, lei="875500ELOZEL05BVXV37")
+        hab, lcrdntler, lsaid, lkmsgs, ltmsgs, limsgs, leimsgs = get_cred(hby, hab, regery, registry, verifier, Schema.LE_SCHEMA1, leicred, seqner)
+
+        #chained ecr auth cred
+        eaedge = get_ecr_auth_edge(lsaid,Schema.LE_SCHEMA1)
+
+        eacred = get_ecr_auth_cred(aid=hab.pre, issuer=hab.pre, recipient=hab.pre, schema=Schema.ECR_AUTH_SCHEMA2, registry=registry, sedge=eaedge, lei="875500ELOZEL05BVXV37")
+        hab, eacrdntler, easaid, eakmsgs, eatmsgs, eaimsgs, eamsgs = get_cred(hby, hab, regery, registry, verifier, Schema.ECR_AUTH_SCHEMA2, eacred, seqner)
+
+        #chained ecr auth cred
+        ecredge = get_ecr_edge(easaid,Schema.ECR_AUTH_SCHEMA2)
+
+        ecr = get_ecr_cred(issuer=hab.pre, recipient=hab.pre, schema=Schema.ECR_SCHEMA, registry=registry, sedge=ecredge, lei="875500ELOZEL05BVXV37")
+        hab, eccrdntler, ecsaid, eckmsgs, ectmsgs, ecimsgs, ecmsgs = get_cred(hby, hab, regery, registry, verifier, Schema.ECR_SCHEMA, ecr, seqner)
+        environment_middleware = start.EnvironmentMiddleware()
+        app = falcon.App(
+            middleware=[falcon.CORSMiddleware(
+                allow_origins='*',
+                allow_credentials='*',
+                expose_headers=['cesr-attachment', 'cesr-date', 'content-type']), environment_middleware])
+        vdb = basing.VerifierBaser(name=hby.name, temp=True)
+        verifying.setup(app=app, hby=hby, vdb=vdb, reger=eccrdntler.rgy.reger)
+        server = http.Server(port=port, app=app)
+        httpServerDoer = http.ServerDoer(server=server)
+        # class testCf:
+        #     def get():
+        #         return dict(LEIs=[f"{LEI1}",f"{LEI2}"])
+        rootsCf = configing.Configer(name="verifier-config-public.json",
+                            headDirPath="/home/aidar/Desktop/git/gleif/vlei-verifier/scripts",
+                            base="",
+                            temp=False, reopen=True, clear=False)
+        authDoers = authorizing.setup(hby, vdb=vdb, reger=eccrdntler.rgy.reger, cf=rootsCf)
+
+        reportDoers = reporting.setup(app=app, hby=hby, vdb=vdb)
+
+        doers = authDoers + reportDoers + [httpServerDoer]
+        limit = 0.25
+        tock = 0.03125
+        doist = doing.Doist(limit=limit, tock=tock)
+        doist.doers = doers
+        doist.enter()
+        # assert len(doist.deeds) == 2
+        # assert [val[1] for val in doist.deeds] == [0.0, 0.0]  #  retymes
+        # for doer in doers:
+        #     assert doer.baser.opened
+        #     assert "_test/keri/db/test" in doer.baser.path
+        try:
+            doist.recur()
+        except Exception as e:
+            raise ValueError(f"Likely you have another service running on {port}")
+
+        issAndCred = bytearray()
+        # issAndCred.extend(kmsgs)
+        # issAndCred.extend(tmsgs)
+        # issAndCred.extend(imsgs)
+        issAndCred.extend(ecmsgs)
+        acdc = issAndCred.decode("utf-8")
+
+        # use this for integration testing debugging sessions
+        while True:
+            time.sleep(1)
+            doist.recur()
 
 def presentation_request(said, acdc, exceptions):
     try:
